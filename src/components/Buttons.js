9a3278018890866cad2b67c67760684081717c17
Radi Galani <radi_d_1@yahoo.de>
2023-11-23 21:24:04 +0100

    changed package.json , with gh-pages

---
+++ src/components/Buttons.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  background-color: darkgray;
+  border: 4px solid gray;
+  border-radius: 10px;
+  color: black;
+  cursor: pointer;
+  font-size: 18px;
+  margin: 8px;
+  padding: 0.6em 2em;
+  position: relative;
+  text-align: center;
+  transition: 1s all ease-out;
+`;
+
+export default StyledButton;

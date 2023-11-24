13a5ae79ce1f1e46ad9657b2c4f4bd8cecfd6952
Radi Galani <radi_d_1@yahoo.de>
2023-11-24 16:16:25 +0100

    modified footer

--- src/Footer.js
+++ src/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer  className= "footer">
         <div>
         <ul className="footer1line">
-          <li><a href="/impresum.html">Impressum</a></li>
+          <li><a href="/impressum.html">Impressum</a></li>
           <li><a href="/contact.html">Contact Us</a></li>
           <li><a href="/index.html" >Spices & Herbs</a></li>
           <li><a href="/shipping.html">Shipping</a></li>
